8324bb8f828f5885e5421317f953099f6bc8c5b3
aslanpour.sadegh <aslanpour.sadegh@gmail.com>
2019-10-13 11:26:14 +0330

    skew and kurtosis

--- src/datasetanalysis/ReadWriteCSV.java
+++ src/datasetanalysis/ReadWriteCSV.java
@@ -12,6 +12,10 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import org.apache.commons.math3.stat.Frequency;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.commons.math3.stat.correlation.Covariance;
+import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
 
 /**
  *
@@ -25,8 +29,9 @@ public class ReadWriteCSV {
      */
     public static void main(String[] args) throws IOException {
         ArrayList dataset = readCSV("src/files/new.csv", true);
-        pickAnItemList(dataset, 4);//get temprature
-        
+        double [] temprature = pickAnItemList(dataset, 4);//get temprature
+        descriptiveStatistics(temprature);
+        writeCSV(temprature, "src/files/new2.csv");
     }
     
     public static double[] pickAnItemList(ArrayList dataList, int index){
@@ -40,6 +45,76 @@ public class ReadWriteCSV {
         return itemList;
     }
     
+    public static int mode(int a[]) {
+    int maxValue = 0, maxCount = 0;
+
+    for (int i = 0; i < a.length; ++i) {
+        int count = 0;
+        for (int j = 0; j < a.length; ++j) {
+            if (a[j] == a[i]) ++count;
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            maxValue = a[i];
+        }
+    }
+
+    return maxValue;
+    }
+    
+    public static double mode(double a[]) {
+        double maxValue = 0, maxCount = 0;
+
+        for (int i = 0; i < a.length; ++i) {
+            int count = 0;
+            for (int j = 0; j < a.length; ++j) {
+                if (a[j] == a[i]) ++count;
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                maxValue = a[i];
+            }
+        }
+        return maxValue;
+    }
+    
+    public static double[] sort(double[] values){
+        DescriptiveStatistics descriptiveStat = new DescriptiveStatistics(values);
+        return descriptiveStat.getSortedValues();
+    }
+    public static void descriptiveStatistics(double[] values){
+        DescriptiveStatistics descriptiveStat = new DescriptiveStatistics(values);
+        double count = values.length;
+        double sum = descriptiveStat.getSum();
+        double min = descriptiveStat.getMin();
+        double max = descriptiveStat.getMax();
+        double mean = descriptiveStat.getMean();
+        double mode =  mode(values);
+        double standardDeviation = descriptiveStat.getStandardDeviation();
+        double skewness = descriptiveStat.getSkewness();
+        double kurtosis = descriptiveStat.getKurtosis();
+        double median = descriptiveStat.getPercentile(50);
+        double percentile99 = descriptiveStat.getPercentile(99);
+        
+        
+        
+        System.out.println("Count: " + count +
+                            "\nSum: " + sum + 
+                            "\nMin: " + min + 
+                            "\nMax: " + max + 
+                            "\nMean: " + mean + 
+                            "\nMode: " + mode +
+                            "\nStandard Deviation: " + standardDeviation + 
+                            "\nSkewness: " + skewness + 
+                            "\nKurtosis: " + kurtosis + 
+                            "\nMedian: " + median +
+                            "\n90th Percentile: " + percentile99);
+    }
+        
+    
+    public static void correlation(double[] x, double[] y){
+        
+    }
     public static void preprocess_mi_meteo_2001(String file) throws IOException{
         ArrayList dataList = new ArrayList<>();
         BufferedReader csvReader;
@@ -137,5 +212,47 @@ public class ReadWriteCSV {
         
         return dataList;
 
+    }
+    
+    public static void writeCSV(double[] data, String fileName) throws IOException{
+        FileWriter csvWriter = new FileWriter(fileName);
+                
+        for (int i = 0; i < data.length; i++) {
+           csvWriter.append(data[i] + "\n");
+        }
+        
+        csvWriter.flush();
+        csvWriter.close();
+    }
+    
+    public static void writeCSV(ArrayList dataList, String fileName) throws IOException{
+        FileWriter csvWriter = new FileWriter(fileName);
+                
+        for (int i = 0; i < dataList.size(); i++) {
+            ArrayList<Double> row = (ArrayList<Double>)dataList.get(i);
+            
+           for (int j = 0; j< row.size(); j++){
+               if (j <row.size() - 1)
+                csvWriter.append(row.get(j) + ",");
+               else
+                   csvWriter.append(String.valueOf(row.get(j)));
+           }
+           csvWriter.append("\n");
+        }
+        
+        csvWriter.flush();
+        csvWriter.close();
+    }
+    
+    public static void writeCSV(double[] data, String fileName, String lable) throws IOException{
+        FileWriter csvWriter = new FileWriter(fileName);
+        csvWriter.append(lable + "\n");
+        
+        for (int i = 0; i < data.length; i++) {
+           csvWriter.append(data[i] + "\n");
+        }
+        
+        csvWriter.flush();
+        csvWriter.close();
     }
 }
