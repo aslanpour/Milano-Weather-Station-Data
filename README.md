df26b1cf847f4c99359f8acd0902ca78d3b1c2a6
aslanpour.sadegh <aslanpour.sadegh@gmail.com>
2019-10-16 12:30:52 +0330

    neural network

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DatasetAnalysis
+${licensePrefix}
---
+++ src/datasetanalysis/Main.java
@@ -0,0 +1,17 @@
+/*
+ * This project analyzes the Milano Weather Station Data. 
+ * This data contains the information about the temperature and relative humidity gathered 
+ * during around 2 month in 2013 in Milano - via Lambrate street.
+ * 1) We 1) analyze the data in terms of its centrality and shape, 
+ * 2) try to find a relationship between temperature and relative humidity using regression, 
+ * and 3) propose a prediction method for this purpose based on artificial neural networks.
+ */
+package datasetanalysis;
+
+/**
+ *
+ * @author aslanpour
+ */
+public class Main {
+    
+}
